bfd5ef29a1ac13ac8d832ed1e4f5aee0387541c2
ft-johan <johangeorge201203@gmail.com>
2025-07-13 17:56:10 +0000

    basic ratatui setup

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,85 @@
-use color_eyre::eyre::{Ok ,Result};
-use ratatui::{crossterm::event::{self, Event}, layout::{Constraint, Layout}, widgets::{Paragraph, Widget}, DefaultTerminal, Frame};
-
-
-#[derive(Debug , Default)]
-struct AppState{
-    items:Vec<TodoItem>
-}}
+use color_eyre::eyre::{Ok, Result};
+use ratatui::{
+    DefaultTerminal, Frame,
+    crossterm::{
+        event::{self, Event},
+        style::Color,
+    },
+    layout::{Constraint, Layout},
+    style::Stylize,
+    symbols::border::ROUNDED,
+    widgets::{Block, BorderType, List, ListItem, Paragraph, Widget},
+};
 
 #[derive(Debug, Default)]
-struct TodoItem{
-    is_done:bool,
-    description:String,
+struct AppState {
+    items: Vec<TodoItem>,
 }
-fn main() -> Result<()>
-  {
-    let mut state =AppState::default();
-    println!("Hello, world!");
+
+#[derive(Debug, Default)]
+struct TodoItem {
+    is_done: bool,
+    description: String,
+}
+fn main() -> Result<()> {
+    let mut state = AppState::default();
+    state.items.push(TodoItem {
+        is_done: false,
+        description: String::from("Finish applications"),
+    });
+    state.items.push(TodoItem {
+        is_done: false,
+        description: String::from("Finish applications"),
+    });
+    state.items.push(TodoItem {
+        is_done: false,
+        description: String::from("Finish applications"),
+    });
+    state.items.push(TodoItem {
+        is_done: false,
+        description: String::from("Finish applications"),
+    });
+  
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let result = run(terminal,&mut state) ;
+    let result = run(terminal, &mut state);
     ratatui::restore();
     result
 }
 
-fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) ->Result<()>
- {
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
-terminal.draw(|f|render(f,app_state))?;
+        terminal.draw(|f| render(f, app_state))?;
 
-        if let Event ::Key(key)= event::read()? {
+        if let Event::Key(key) = event::read()? {
             match key.code {
-                event::KeyCode::Esc =>{
+                event::KeyCode::Esc => {
                     break;
                 }
                 _ => {}
             }
-            
         }
     }
     Ok(())
 }
 
+fn render(frame: &mut Frame, app_state: &AppState) {
+    let [border_area] = Layout::vertical([Constraint::Fill((1))])
+        .margin(1)
+        .areas(frame.area());
+    let [inner_area] = Layout::vertical([Constraint::Fill((1))])
+        .margin(1)
+        .areas(border_area);
 
-fn render(frame:&mut Frame,  app_state:&AppState) {
-    let [border_area] = Layout::vertical([Constraint::Fill((1))]).margin(1).areas(frame.area()) ;
-    Paragraph::new("Hello from application").render(frame.area(), frame.buffer_mut());
+    Block::bordered()
+        .border_type(BorderType::Rounded)
+        .fg(Color::Yellow)
+        .render(border_area, frame.buffer_mut());
+    List::new(
+        app_state
+            .items
+            .iter()
+            .map(|x| ListItem::from(x.description.clone())),
+    )
+    .render(inner_area, frame.buffer_mut());
 }
