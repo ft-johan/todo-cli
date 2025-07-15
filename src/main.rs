7e87b20f180a5a24aca4ad7c91df5715d416141b
ft-johan <johangeorge201203@gmail.com>
2025-07-15 14:28:17 +0000

    big fix

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use color_eyre::eyre::{Ok, Result};
+use std::char;
+
+use color_eyre::{eyre::{Ok, Result}};
 use ratatui::{
-    DefaultTerminal, Frame,
     crossterm::{
         event::{self, Event},
         style::Color,
-    },
-    layout::{Constraint, Layout},
-    style::Stylize,
-    symbols::border::ROUNDED,
-    widgets::{Block, BorderType, List, ListItem, Paragraph, Widget},
+    }, layout::{Constraint, Layout}, style::{Style, Stylize}, symbols::border::ROUNDED, widgets::{Block, BorderType, List, ListItem, ListState, Paragraph, Widget}, DefaultTerminal, Frame
 };
 
 #[derive(Debug, Default)]
 struct AppState {
     items: Vec<TodoItem>,
+    list_state: ListState,
+    is_add_new:bool,
 }
 
 #[derive(Debug, Default)]
@@ -23,6 +22,8 @@ struct TodoItem {
 }
 fn main() -> Result<()> {
     let mut state = AppState::default();
+    state.is_add_new=false;
+
     state.items.push(TodoItem {
         is_done: false,
         description: String::from("Finish applications"),
@@ -39,7 +40,7 @@ fn main() -> Result<()> {
         is_done: false,
         description: String::from("Finish applications"),
     });
-  
+
     color_eyre::install()?;
     let terminal = ratatui::init();
     let result = run(terminal, &mut state);
@@ -56,6 +57,24 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
                 event::KeyCode::Esc => {
                     break;
                 }
+                event::KeyCode::Char(char) => match char {
+                    'A' =>{
+                        app_state.is_add_new=true;
+                    }
+                    'D' =>{
+                        if let Some(index) = app_state.list_state.selected(){
+                            app_state.items.remove(index);
+                        }
+                    }
+                    'k' => {
+                        app_state.list_state.select_previous();
+                    }
+                    'j' => {
+                        app_state.list_state.select_next();
+                    }
+                    _ => {}
+                },
+
                 _ => {}
             }
         }
@@ -63,7 +82,7 @@ fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, app_state: &AppState) {
+fn render(frame: &mut Frame, app_state: &mut AppState) {
     let [border_area] = Layout::vertical([Constraint::Fill((1))])
         .margin(1)
         .areas(frame.area());
@@ -75,11 +94,13 @@ fn render(frame: &mut Frame, app_state: &AppState) {
         .border_type(BorderType::Rounded)
         .fg(Color::Yellow)
         .render(border_area, frame.buffer_mut());
-    List::new(
+    let list = List::new(
         app_state
             .items
             .iter()
             .map(|x| ListItem::from(x.description.clone())),
     )
-    .render(inner_area, frame.buffer_mut());
+    .highlight_symbol(">")
+    .highlight_style(Style::default().fg(Color::Black.into()));
+    frame.render_stateful_widget(list, inner_area, &mut app_state.list_state);
 }
